abf1e537fa32624185897376a20e5ee84ccc8360
hamstercat007 <lisamac421@gmail.com>
2022-06-21 10:47:29 +0100

    Resetting main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import Main from "../Main";
 
 function App() {
   return (
     <div className="App">
       <h1>SoC Survival Guide</h1>
       <p>Helping bootcampers get through the journey</p>
-      <Main />
     </div>
   );
 }
