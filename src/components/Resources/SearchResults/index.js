4285340241f61dfe66eca527ebb38ebfca57c635
St0neofFr33dom <christophefreeston@googlemail.com>
2022-06-23 10:35:39 +0100

    Added results imiter and buttons to scroll through

--- src/components/Resources/SearchResults/index.js
+++ src/components/Resources/SearchResults/index.js
@@ -1,15 +1,47 @@
 //render search results
 import ResultItem from "../ResultItem";
-import "./index.css"
+import "./index.css";
+import {useState} from "react";
 
 function SearchResults({ searchResults }) {
-  return searchResults.map(function (item, index) {
-    return (
-      <div className="searchResults" >
-    <ResultItem properties={item} key={index} />
+  const [position, setPosition] = useState(0)
+
+  let displayedArray = searchResults.slice(position,position+7)
+
+  function nextButton(){
+    if (position < searchResults.length){
+      setPosition(position+7)
+    } else{
+      return
+    }
+  }
+
+  function previousButton(){
+    if (position > 0){
+      setPosition(position-7)
+    } else{
+      return
+    }
+  }
+
+  function createResultItem(item, index){
+    return<ResultItem properties={item} key={index} />
+  }
+  return (
+    <div className="searchResults">
+    {displayedArray.map(createResultItem)}
+    <button type="button" onClick={previousButton}>Previous</button>
+    <button type="button" onClick={nextButton}>Next</button>
     </div>
-    )
-  });
+ 
+  );
 }
 
 export default SearchResults;
+
+
+//Take in an array of objects
+//it maps out a componenet for each object
+//Make a new array which is a slice of the original array
+//map that array 
+//change the slice index via state vairable, which is controlled by buttons
