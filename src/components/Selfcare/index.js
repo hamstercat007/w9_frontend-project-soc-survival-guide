73b28dec1a4b8c6eb33fd710d11974014ce58865
hamstercat007 <lisamac421@gmail.com>
2022-06-23 19:01:18 +0100

    Other resources included

--- src/components/Selfcare/index.js
+++ src/components/Selfcare/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import squats from "./squats.jpg";
+import "./index.css";
 
 import MyTimer from "../MyTimer";
 function SelfCare() {
@@ -14,9 +15,42 @@ function SelfCare() {
             <img style={{ height: "45vh" }} src={squats} alt="squats" />
             <p>Do your 2 minutes of squats before hitting the resources!</p>
           </div>
-          <div style={{marginLeft: "2rem"}}>
-          <MyTimer setTimesUp={setTimesUp} />
+          <div style={{ marginLeft: "2rem" }}>
+            <MyTimer setTimesUp={setTimesUp} />
           </div>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h2>Other Resources</h2>
+          <ul>
+            <a
+              href="https://www.youtube.com/watch?v=ngQxxLnJ-5I"
+              style={{ textDecoration: "none" }}
+            >
+              <li
+                style={{
+                  marginBottom: "1rem",
+                  fontSize: "2rem",
+                  listStyle: "none",
+                }}
+              >
+                Yoga: 10 mins of yoga
+              </li>
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=EvTJqhVfBR4"
+              style={{ textDecoration: "none" }}
+            >
+              <li
+                style={{
+                  marginBottom: "1rem",
+                  fontSize: "2rem",
+                  listStyle: "none",
+                }}
+              >
+                Qigong - aids breathing and calms the mind
+              </li>
+            </a>
+          </ul>
         </div>
       </div>
     );
