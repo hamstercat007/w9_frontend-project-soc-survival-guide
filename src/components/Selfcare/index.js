fa460f98193a6a36e38147f247f6fc8daa30351a
CodeNameMoe <newmoeali@gmail.com>
2022-06-23 15:02:58 +0100

    added ul and css

--- src/components/Selfcare/index.js
+++ src/components/Selfcare/index.js
@@ -1,5 +1,6 @@
 import MyTimer from "../MyTimer";
 import {useState} from "react"
+import "./index.css"
 
 function SelfCare() {
   const [customTime, setCustomTime] = useState(20)
@@ -16,6 +17,24 @@ function SelfCare() {
       <MyTimer expiryTimestamp={time} calculateTime={calculateTime} />
       <p>Timer set for:</p>
       <h2>{customTime}</h2>
+      <div className="sub-headings"> 
+        <h3>Mind</h3>
+        <h3>Body</h3>
+      </div>
+      <div className="activites">
+        <ul>
+          <a href="https://www.youtube.com/watch?v=QM8NjfCfOg0" target="_blank" rel="noreferrer"><li>Yoga</li></a>
+          <a href="https://www.youtube.com/watch?v=y2RAEnWreoE" target="_blank" rel="noreferrer" ><li>Qi Gong</li></a>
+          <a href="https://www.youtube.com/watch?v=8plwv25NYRo" target="_blank" rel="noreferrer"><li>Meditation</li></a>
+        </ul>
+        <ul>
+          
+          <li>Squats</li>
+          <li>Lunges</li>
+          <li>Star Jumps</li>
+
+        </ul>
+      </div>
     </div>
   );
 }
