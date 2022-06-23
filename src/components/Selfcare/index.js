9387da9636e52ffa1c6bffeb3ae1af384766b9a9
MiguelLamas <lamasmigs@yahoo.co.uk>
2022-06-23 18:33:10 +0100

    squats

--- src/components/Selfcare/index.js
+++ src/components/Selfcare/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import squats from "./squats.jpg";
 
 import MyTimer from "../MyTimer";
 function SelfCare() {
@@ -8,7 +9,15 @@ function SelfCare() {
     return (
       <div>
         <h1>Take care of yourself</h1>
-        <MyTimer setTimesUp={setTimesUp} />
+        <div style={{ display: "flex" }}>
+          <div>
+            <img style={{ height: "45vh" }} src={squats} alt="squats" />
+            <p>Do your 2 minutes of squats before hitting the resources!</p>
+          </div>
+          <div style={{marginLeft: "2rem"}}>
+          <MyTimer setTimesUp={setTimesUp} />
+          </div>
+        </div>
       </div>
     );
   } else {
