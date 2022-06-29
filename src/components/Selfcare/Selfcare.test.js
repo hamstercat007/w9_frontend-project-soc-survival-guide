373e4293c88137602338a1b3b4b705c651df2f9e
hamstercat007 <lisamac421@gmail.com>
2022-06-29 16:05:31 +0100

    deleted comments

--- src/components/Selfcare/Selfcare.test.js
+++ src/components/Selfcare/Selfcare.test.js
@@ -1,21 +1,17 @@
-import { fireEvent, render, screen } from "@testing-library/react";
-import { test, jest, expect } from "@jest/globals";
+import { render, screen } from "@testing-library/react";
+import { test, expect } from "@jest/globals";
 import SelfCare from "./index.js";
 
 test("A link renders on the page", () => {
   render(<SelfCare />);
   const yogaLink = screen.getByText(/Yoga/);
-  expect(yogaLink).toBeInTheDocument()
-
+  expect(yogaLink).toBeInTheDocument();
 });
 
 test("the yoga link has the yoga video address", () => {
   render(<SelfCare />);
-  expect(screen.getByTestId('yoga1')).toHaveAttribute('href', 'https://www.youtube.com/watch?v=ngQxxLnJ-5I');
-})
-
-
-// expect(link).closest("a").toHaveAttribute('href', 'https://www.youtube.com/watch?v=ngQxxLnJ-5I')
-
-
-// expect(screen.getByText('Click Me').closest('a')).toHaveAttribute('href', 'https://www.test.com/')
+  expect(screen.getByTestId("yoga1")).toHaveAttribute(
+    "href",
+    "https://www.youtube.com/watch?v=ngQxxLnJ-5I"
+  );
+});
